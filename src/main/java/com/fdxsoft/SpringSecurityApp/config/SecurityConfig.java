6dba4df20852989c7ffdb4accc4674dd4e1e0781
Federico Martinez <fedemtzesc@hotmail.com>
2025-11-16 18:30:36 -0600

    Se reorganizaron los imports

--- src/main/java/com/fdxsoft/SpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/fdxsoft/SpringSecurityApp/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.fdxsoft.SpringSecurityApp.config;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,13 +13,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.fdxsoft.SpringSecurityApp.service.UserDetailsServiceImpl;
